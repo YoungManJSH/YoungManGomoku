4c73db659f4a09e4852072baab42165a8583d288
Talent806 <owen80610@gmail.com>
2025-12-01 16:03:41 +0900

    [feat/Ingame]: InGame Client Library Update
    251201
     - Library Source Code - 금수 프로그램 라이브러리 추가

---
+++ YoungManGomoku_Client/Assets/Scripts/SceneScripts/InGame/Board.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using UnityEngine;
+
+public class Board
+{
+    public const int BoardSize = 15;
+    public const int MaxCoord = BoardSize - 1;
+
+    private readonly Stone[,] _nowBoard;
+    private readonly JudgeType[,] _blackJudges;
+
+    public int NowTurn { get; private set; }
+
+    public Stone this[int row, int col] => _nowBoard[row, col];
+
+    public Board()
+    {
+        _nowBoard = new Stone[BoardSize, BoardSize];
+        _blackJudges = new JudgeType[BoardSize, BoardSize];
+        NowTurn = 0;
+    }
+
+    public bool MoveStone(int row, int col)
+    {
+        if (NowTurn % 2 == 0)
+        {
+            if (MoveBlack(row, col) is false)
+                return false;
+        }
+        else
+        {
+            MoveWhite(row, col);
+            if (NowTurn > 6) // 흑돌이 4개 이상 있고 백돌 착수 이후
+            {
+                UpdateBlackJudges();
+            }
+        }
+
+        ++NowTurn;
+
+        // 테스트용 출력
+        Console.WriteLine($"현재 {NowTurn}수 진행 중...");
+        return true;
+    }
+
+    private void UpdateBlackJudges()
+    {
+        for (int row = 0; row < BoardSize; ++row)
+        {
+            for (int col = 0; col < BoardSize; ++col)
+            {
+                if (_nowBoard[row, col] != Stone.Empty)
+                {
+                    _blackJudges[row, col] = JudgeType.None;
+                    continue;
+                }
+
+                _blackJudges[row, col] = JudgeMove.JudgeBlackMove(this, row, col);
+            }
+        }
+    }
+
+    private bool MoveBlack(int row, int col)
+    {
+        Debug.Assert(0 <= row && row < BoardSize);
+        Debug.Assert(0 <= col && col < BoardSize);
+        Debug.Assert(_nowBoard[row, col] == Stone.Empty);
+
+        if (_blackJudges[row, col] == JudgeType.Forbidden)
+        {
+            Console.WriteLine($"[{row}, {col}] 위치는 흑돌 금수입니다.");
+            return false;
+        }
+
+        _nowBoard[row, col] = Stone.Black;
+
+        // 테스트용 출력
+        Console.WriteLine($"흑돌 [{row}, {col}] 착수");
+        if (_blackJudges[row, col] == JudgeType.Omok)
+        {
+            Console.WriteLine("*** 흑돌 오목! ***");
+        }
+
+        return true;
+    }
+
+    private void MoveWhite(int row, int col)
+    {
+        Debug.Assert(0 <= row && row < BoardSize);
+        Debug.Assert(0 <= col && col < BoardSize);
+        Debug.Assert(_nowBoard[row, col] == Stone.Empty);
+
+        _nowBoard[row, col] = Stone.White;
+
+        // 테스트용 출력
+        Console.WriteLine($"백돌 [{row}, {col}] 착수");
+        if (JudgeMove.JudgeWhiteOmok(this, row, col))
+        {
+            Console.WriteLine("*** 백돌 오목! ***");
+        }
+    }
+}
---
+++ YoungManGomoku_Client/Assets/Scripts/SceneScripts/InGame/JudgeType.cs
@@ -0,0 +1,6 @@
+﻿public enum JudgeType
+{
+    None,
+    Omok,
+    Forbidden
+}
