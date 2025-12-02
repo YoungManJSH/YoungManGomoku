28e1a5af7216fa7fb753e9bba6660080a5f272da
Talent806 <owen80610@gmail.com>
2025-12-02 16:09:09 +0900

    [feat/Ingame]: InGame Client Scene Update
    - BoardGenerator 스크립트 작성
    - 해상도에 따른 board Scale 조정
    - 격자 및 Star Point(화점) 형성

--- YoungManGomoku_Client/Assets/Scripts/SceneScripts/InGame/BoardGenerator.cs
+++ YoungManGomoku_Client/Assets/Scripts/SceneScripts/InGame/BoardGenerator.cs
@@ -2,23 +2,38 @@ using UnityEngine;
 
 public class BoardGenerator : MonoBehaviour
 {
+    private const float PPU = 100f; //Pixels per unit
+
     [SerializeField] private Texture2D woodTexture;
     [SerializeField] private int cellSize;
     [SerializeField] private int marginSize;
     [SerializeField] private int lineThickness;
+    [SerializeField] private int pointRadius;
     [SerializeField] private Color lineColor;
 
-    private Renderer _renderer;
+    private SpriteRenderer _spriteRenderer;
 
     private void Start()
     {
-        _renderer = GetComponent<Renderer>();
-        _renderer.material.mainTexture = GenerateBoard();
+        int totalPixel = cellSize * Board.MaxCoord + marginSize * 2;
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = GenerateBoard(totalPixel);
+        
+        AdjustBoardScale(totalPixel);
     }
 
-    private Texture2D GenerateBoard()
+    private void AdjustBoardScale(int totalPixel)
     {
-        int totalPixel = cellSize * Board.MaxCoord + marginSize * 2;
+        float worldSize = totalPixel / PPU;
+        float screenWidth = Camera.main!.orthographicSize * 2f * Camera.main.aspect;
+        float scale = screenWidth / worldSize;
+        transform.localScale = new Vector3(scale, scale, 1f);
+    }
+
+    private Sprite GenerateBoard(int totalPixel)
+    {
+        int endGridPos = totalPixel - marginSize;
         Texture2D boardTexture = new Texture2D(totalPixel, totalPixel, TextureFormat.RGBA32, false);
 
         // woodTexture(배경) 적용
@@ -31,30 +46,38 @@ public class BoardGenerator : MonoBehaviour
         }
 
         // 격자선 적용
-        for (int i = 0; i < Board.BoardSize; ++i)
+        DrawLineHor(boardTexture, marginSize, endGridPos, marginSize, lineThickness * 2, lineColor);
+        DrawLineHor(boardTexture, marginSize, endGridPos, endGridPos, lineThickness * 2, lineColor);
+        DrawLineVer(boardTexture, marginSize, marginSize, endGridPos, lineThickness * 2, lineColor);
+        DrawLineVer(boardTexture, endGridPos, marginSize, endGridPos, lineThickness * 2, lineColor);
+        for (int i = 1; i < Board.MaxCoord; ++i)
         {
-            int pos = i * cellSize + marginSize;
-            DrawLineHor(boardTexture, 0, totalPixel, pos, lineThickness, lineColor);
-            DrawLineVer(boardTexture, pos, 0, totalPixel, lineThickness, lineColor);
+            int pos = marginSize + cellSize * i;
+            DrawLineHor(boardTexture, marginSize, endGridPos, pos, lineThickness, lineColor);
+            DrawLineVer(boardTexture, pos, marginSize, endGridPos, lineThickness, lineColor);
         }
-        
+
         // 화점 적용
         int center = marginSize + Board.MaxCoord / 2 * cellSize;
         int point1 = marginSize + 3 * cellSize;
         int point2 = totalPixel - point1;
-        DrawCircle(boardTexture, center, center, 6, lineColor);
-        DrawCircle(boardTexture, point1, point1, 6, lineColor);
-        DrawCircle(boardTexture, point1, point2, 6, lineColor);
-        DrawCircle(boardTexture, point2, point1, 6, lineColor);
-        DrawCircle(boardTexture, point2, point2, 6, lineColor);
+        DrawCircle(boardTexture, center, center, pointRadius, lineColor);
+        DrawCircle(boardTexture, point1, point1, pointRadius, lineColor);
+        DrawCircle(boardTexture, point1, point2, pointRadius, lineColor);
+        DrawCircle(boardTexture, point2, point1, pointRadius, lineColor);
+        DrawCircle(boardTexture, point2, point2, pointRadius, lineColor);
+
+        boardTexture.Apply();
+        boardTexture.filterMode = FilterMode.Point;
         
-        return boardTexture;
+        return Sprite.Create(boardTexture, new Rect(0, 0, totalPixel, totalPixel),
+            new Vector2(0.5f, 0.5f), PPU);
     }
 
     private void DrawLineHor(Texture2D tex, int x0, int x1, int y, int thickness, Color color)
     {
         Debug.Assert(x0 <= x1);
-        
+
         for (int x = x0; x <= x1; ++x)
         {
             for (int ty = -thickness; ty <= thickness; ++ty)
