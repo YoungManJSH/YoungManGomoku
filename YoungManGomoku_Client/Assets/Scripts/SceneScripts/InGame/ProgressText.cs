18b4a19170c94b95fa5228485737847a795edeba
Talent806 <owen80610@gmail.com>
2025-12-09 16:17:30 +0900

    [feat/Ingame]: Project Setting Update
    - Script Execution Order : GameManager(-2), EventManager(-1)
    - Player → Resolution and Presentation
      PC 버전 : Run In Background(true), FullscreenMode(Windowed), Defualt Screen Width/Height(1080/2340), Allow Fullscreen Switch(false), Force Single Instance(true)
      안드로이드 버전 : Run Without Focus(true), Minimum Window Width/Height(540, 1200), Resolution Scaling Mode(Letterboxed)

--- YoungManGomoku_Client/Assets/Scripts/SceneScripts/InGame/ProgressText.cs
+++ YoungManGomoku_Client/Assets/Scripts/SceneScripts/InGame/ProgressText.cs
@@ -3,32 +3,28 @@ using UnityEngine;
 
 public class ProgressText : MonoBehaviour
 {
-    [SerializeField] private GameManager gameManager;
-    [SerializeField] private EventManager eventManager;
-    
     private TextMeshProUGUI _progressText;
     
     private void Awake()
     {
         _progressText = GetComponent<TextMeshProUGUI>();
 
-        eventManager.OnPlayerSurrender += () => OnGameOver("기권패");
-        eventManager.OnOppositeSurrender += () => OnGameOver("기권승");
-        eventManager.OnOppositeDisconnectedWin += () => OnGameOver("접속끊김승");
-    }
-
-    public void OnBoardGenerated(Board board, bool isPlayerBlack, GameManager.TimeController playerTime,  GameManager.TimeController oppositeTime)
-    {
-        board.OnTurnChanged += OnTurnChanged;
-        board.BlackWin += () => OnGameOver(isPlayerBlack ? "승리" : "패배");
-        board.WhiteWin += () => OnGameOver(isPlayerBlack ? "패배" : "승리");
-        playerTime.OnTimeLose += () => OnGameOver("시간패");
-        oppositeTime.OnTimeLose += () => OnGameOver("시간승"); // 추후 수정, 시간패는 서버 처리 받아야 함
+        GameManager gm = GameManager.Instance;
+        gm.BoardInform.OnTurnChanged += OnTurnChanged;
+        gm.BoardInform.BlackWin += () => OnGameOver(gm.IsPlayerBlack ? "승리" : "패배");
+        gm.BoardInform.WhiteWin += () => OnGameOver(gm.IsPlayerBlack ? "패배" : "승리");
+        gm.PlayerTime.OnTimeLose += () => OnGameOver("시간패");
+        gm.OppositeTime.OnTimeLose += () => OnGameOver("시간승"); // 추후 수정, 시간패는 서버 처리 받아야 함
+        
+        EventManager em = EventManager.Instance;
+        em.OnPlayerSurrender += () => OnGameOver("기권패");
+        em.OnOppositeSurrender += () => OnGameOver("기권승");
+        em.OnOppositeDisconnectedWin += () => OnGameOver("접속끊김승");
     }
     
     private void OnTurnChanged(int turn)
         => _progressText.text = $"{turn}수 진행 중";
 
     private void OnGameOver(string gameResult) 
-        => _progressText.text = $"{(gameManager.IsPlayerBlack ? "흑" : "백")} {gameManager.BoardInform.NowTurn}수 {gameResult}";
+        => _progressText.text = $"{(GameManager.Instance.IsPlayerBlack ? "흑" : "백")} {GameManager.Instance.BoardInform.NowTurn}수 {gameResult}";
 }
