4f442457d67e4708c39fd406ea82a891ef63a55b
김재환 <kimjh940616@gmail.com>
2025-12-16 22:45:40 +0900

    [Feat/Server]: Fix NetworkManager
    20251216
     - Fix NetworkManager Issue
     - Modify Json String Exception

--- YoungManGomoku_Client/Assets/Scripts/NetworkScripts/NetworkManager.cs
+++ YoungManGomoku_Client/Assets/Scripts/NetworkScripts/NetworkManager.cs
@@ -39,11 +39,15 @@ public class NetworkManager : MonoBehaviour
     /// 회원 가입을 위한 데이터 : 유저 닉네임, ID Token(인증서), guest 여부. 
     /// 구글 계정인 경우 GoogleSignInUser의 IdToken과 DisplayName 사용
     /// </param>
+    /// <param name="timeOutSeconds">
+    /// 웹서버로부터 지정된 시간까지 응답이 없다면 Connection Error를 띄움
+    /// 0이나 음수 값 설정 시 Connection Error 없이 무한 응답 대기
+    /// </param>
     /// <returns>
     /// null : 이미 존재하는 ID Token이라 계정 등록 실패
     /// not null : 회원가입 성공, 계정 정보 받아옴
     /// </returns>
-    public async Awaitable<PlayerData> RegisterAccountRequest(CS_AccountRegisterDTO registerUserDTO)
+    public async Awaitable<PlayerData> RegisterAccountRequest(CS_AccountRegisterDTO registerUserDTO, int timeOutSeconds = 0)
 	{
 		if (registerUserDTO == null)
 		{
@@ -51,67 +55,85 @@ public class NetworkManager : MonoBehaviour
 			return null;
 		}
         // Debug.Log($"Account Register User DTO : {JsonUtility.ToJson(registerUserDTO)}");
-        return await RequestPostServer<PlayerData>("Account/Register", JsonUtility.ToJson(registerUserDTO), "Register Success");
+        return await RequestPostServer<PlayerData>("Account/Register", JsonUtility.ToJson(registerUserDTO), timeOutSeconds, "Account Register Success");
     }
 
 	/// <summary>
 	/// 웹 서버로 해당하는 ID Token의 데이터에 따라 로그인 요청
 	/// </summary>
 	/// <param name="idToken"> 계정 인증용 ID Token. 구글 계정인 경우 GoogleSignInUser.IdToken 사용 </param>
+	/// <param name="timeOutSeconds">
+	/// 웹서버로부터 지정된 시간까지 응답이 없다면 Connection Error를 띄움
+	/// 0이나 음수 값 설정 시 Connection Error 없이 무한 응답 대기
+	/// </param>
 	/// <returns>
 	/// null : 서버 터짐
 	/// </returns>
-	public async Awaitable<PlayerData> LoginRequest(string idToken)
-        => await RequestPostServer<PlayerData>("Account/Login", $"\"{idToken}\"", "Login Success");
+	public async Awaitable<PlayerData> LoginRequest(string idToken, int timeOutSeconds = 0)
+        => await RequestPostServer<PlayerData>("Account/Login", $"\"{idToken}\"", timeOutSeconds, "Login Success");
 
-    /// <summary>
+	/// <summary>
 	/// 웹 서버에 ID Token으로 매칭 등록
 	/// </summary>
 	/// <param name="idToken"> 계정 인증용 ID Token. 구글 계정인 경우 GoogleSignInUser.IdToken 사용 </param>
+	/// <param name="timeOutSeconds">
+	/// 웹서버로부터 지정된 시간까지 응답이 없다면 Connection Error를 띄움
+	/// 0이나 음수 값 설정 시 Connection Error 없이 무한 응답 대기
+	/// </param>
 	/// <returns>
 	/// null : 서버 터짐
 	/// </returns>
-    public async Awaitable<PlayerData> RegisterMatchingRequest(string idToken)
-        => await RequestPostServer<PlayerData>("Matching/Register", $"\"{idToken}\"", "Match Register");
+	public async Awaitable<PlayerData> RegisterMatchingRequest(string idToken, int timeOutSeconds = 0)
+        => await RequestPostServer<PlayerData>("Matching/Register", $"\"{idToken}\"", timeOutSeconds, "Match Register");
 
 
-    /// <summary>
+	/// <summary>
 	/// 웹 서버에 ID Token으로 등록한 매칭 취소
 	/// </summary>
 	/// <param name="idToken"> 계정 인증용 ID Token. 구글 계정인 경우 GoogleSignInUser.IdToken 사용 </param>
+	/// <param name="timeOutSeconds">
+	/// 웹서버로부터 지정된 시간까지 응답이 없다면 Connection Error를 띄움
+	/// 0이나 음수 값 설정 시 Connection Error 없이 무한 응답 대기
+	/// </param>
 	/// <returns>
 	/// null : 서버 터짐
 	/// </returns>
-    public async Awaitable<PlayerData> CancelMatchingRequest(string idToken)
-        => await RequestPostServer<PlayerData>("Matching/Cancel", $"\"{idToken}\"", "Match Cancel");
+	public async Awaitable<PlayerData> CancelMatchingRequest(string idToken, int timeOutSeconds = 0)
+        => await RequestPostServer<PlayerData>("Matching/Cancel", $"\"{idToken}\"", timeOutSeconds, "Match Cancel");
 
 
     // 앞으로 네트워크 매니저의 중추를 담당할 함수들. Open되어있지는 않음.
     // API들은 전부 이 함수들을 Wrapping해 사용할 것
-    private async Awaitable<RecvData> RequestPostServer<RecvData>(string serverURL, string sendJsonString, string successAnnounce = "=== Request Success! ===")
-        => await RequestServer<RecvData>(serverURL, "POST", sendJsonString, successAnnounce);
+    private async Awaitable<RecvData> RequestPostServer<RecvData>(string serverURL, string sendJsonString, int timeOutSeconds = 0, string successAnnounce = "=== Request Success! ===")
+        => await RequestServer<RecvData>(serverURL, "POST", sendJsonString, timeOutSeconds, successAnnounce);
 
     // 서버가 살았는지 아닌지 테스트하는 용도, 핑을 그냥 던져봄. 문자열 퐁이 돌아올 거임.
-    private async Awaitable<RecvData> RequestPing<RecvData>(string successAnnounce = "=== Ping Pong Success! ===")
-        => await RequestServer<RecvData>("Ping", "GET", successAnnounce);
+    private async Awaitable<RecvData> RequestPing<RecvData>(int timeOutSeconds = 10, string successAnnounce = "=== Ping Pong Success! ===")
+        => await RequestServer<RecvData>("Ping", "GET", "\"\"", timeOutSeconds, successAnnounce);
 
-    private async Awaitable<RecvData> RequestServer<RecvData>(string serverURL, string method, string sendJsonString, string successAnnounce = "=== Request Success! ===")
+    private async Awaitable<RecvData> RequestServer<RecvData>(string serverURL, string method, string sendJsonString, int timeOutSeconds = 0, string successAnnounce = "=== Request Success! ===")
 	{
         UnityWebRequest uwr = new UnityWebRequest($"{baseURL}/{serverURL}", method);
 
         //Debug.Log($"Request URL: {baseURL}/{serverURL}\nSend Json Data : {sendJsonString}");
 
-        if (string.IsNullOrEmpty(sendJsonString) || method == "GET")
+        if (string.IsNullOrEmpty(sendJsonString) == false && method != "GET")
         {
-            Debug.Log($"\"Get\" Or Empty Send String : {Encoding.UTF8.GetBytes(sendJsonString).ToString()}");
             uwr.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(sendJsonString));
         }
-        uwr.downloadHandler = new DownloadHandlerBuffer();
+        else
+        {
+            Debug.Log($"\"Get\" Or Empty Send String : {Encoding.UTF8.GetBytes(sendJsonString).ToString()}");
+        }
+
+		uwr.downloadHandler = new DownloadHandlerBuffer();
 
         uwr.SetRequestHeader("Content-Type", "application/json");
 
+        // timeout 지정된 값 만큼 초를 대기하고, 이 시간초 이후에도 응답 없으면 ConnectionError
+        // 0 이하의 값인 경우 timeout 등록을 하지 않고 그냥 무한 응답 대기
+        if (timeOutSeconds > 0) uwr.timeout = timeOutSeconds;
 
-        // uwr.timeout = 10;
         await uwr.SendWebRequest();
 
         if (uwr.result is UnityWebRequest.Result.ConnectionError or UnityWebRequest.Result.ProtocolError)
@@ -138,21 +160,4 @@ public class NetworkManager : MonoBehaviour
         // JsonUtility는 Dictionary와 Property 인식이 불가능하니 주의
         return JsonUtility.FromJson<RecvData>(responseJson);
     }
-
-    // 리팩토링 도중 소멸한 API들
-    [Obsolete("이 메서드는 제거되었습니다. RegisterAccountRequest(CS_AccountRegisterDTO registerUserDTO)를 사용하세요.", true)]
-    public async Awaitable<PlayerData> GuestAccountRegisterRequest(string idToken) 
-        => await RequestPostServer<PlayerData>("/Account/Register", $"\"{idToken}\"", "Register Success");
-
-    [Obsolete("이 메서드는 제거되었습니다. RegisterAccountRequest(CS_AccountRegisterDTO registerUserDTO)를 사용하세요.", true)]
-    public async Awaitable<PlayerData> GoogleAccountRegisterRequest(CS_AccountRegisterDTO GoogleLoginUserDTO)
-        => await RegisterAccountRequest(GoogleLoginUserDTO);
-
-    [Obsolete("이 메서드는 제거되었습니다. LoginRequest(string idToken)를 사용하세요.", true)]
-    public async Awaitable<PlayerData> GuestLoginRequest(string idToken)
-        => await RequestPostServer<PlayerData>("/Account/Login", $"\"{idToken}\"", "Login Success");
-
-    [Obsolete("이 메서드는 제거되었습니다. LoginRequest(string idToken)를 사용하세요.", true)]
-    public async Awaitable<PlayerData> GoogleLoginRequest(string idToken)
-        => await RequestPostServer<PlayerData>("/Account/Login", $"\"{idToken}\"", "Login Success");
 }
