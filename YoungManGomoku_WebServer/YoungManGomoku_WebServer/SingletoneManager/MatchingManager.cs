d63b3b6fa39491f18cba60de036a597ef0c94e69
Talent806 <owen80610@gmail.com>
2026-01-21 07:46:08 +0900

    [feature:Ingame]: Web Server Matching Logic Bug Fix
    - 매칭 취소 → 매칭 재요청 상황에서 매칭 응답이 되지 않던 문제 수정

--- YoungManGomoku_WebServer/YoungManGomoku_WebServer/SingletoneManager/MatchingManager.cs
+++ YoungManGomoku_WebServer/YoungManGomoku_WebServer/SingletoneManager/MatchingManager.cs
@@ -39,7 +39,10 @@ namespace YoungManGomoku_WebServer.SingletoneManager
         // 언제 끝났다고 할 지를 내가 결정하기 위해 사용
         public TaskCompletionSource<MatchResult>? TaskCompSrc { get; set; }
         public CancellationTokenRegistration CancellationTokenRegist { get; set; }
-
+        /* TODO: GPT가 Cancel이나 매칭 성공 때 이거 Dispose 해줘야 한다는데?
+         * IDisposable이라 메모리 누수 이슈 있다는데 제가 막 해도 되는 건지
+         * 잘 몰라서 일단 내버려 뒀습니다. 나중에 확인하시고 맞으면 ㄱㄱ */
+        
         public WaitingPlayer(string playerIdToken, TaskCompletionSource<MatchResult>? taskCompSrc, CancellationTokenRegistration cancellationTokenRegist)
 		{
 			PlayerIdToken = playerIdToken;
@@ -52,11 +55,12 @@ namespace YoungManGomoku_WebServer.SingletoneManager
     public class MatchingManager
     {
         private const int MAX_WAITING = 100;
+        private static readonly Random Random = new Random();
 
         private readonly object _lock;
 
         // 매칭 큐와 대기자 맵은 한 쌍(커플링)이라 Concurrent Collection으로 대체해선 안 된다
-        private readonly Queue<WaitingPlayer> _matchingQueue;
+        private readonly Queue<string> _matchingQueue;
         private readonly Dictionary<string, WaitingPlayer> _waitingMap;
 
         private readonly ILogger<ServerManager> _logger;
@@ -68,7 +72,7 @@ namespace YoungManGomoku_WebServer.SingletoneManager
             _logger = logger;
             _lock = new object();
 
-            _matchingQueue = new Queue<WaitingPlayer>();
+            _matchingQueue = new Queue<string>();
             _waitingMap = new Dictionary<string, WaitingPlayer>();
             _serverManagerContext = serverManagerContext;
             _gameRoomManager = gameRoomManager;
@@ -113,15 +117,10 @@ namespace YoungManGomoku_WebServer.SingletoneManager
                     = new TaskCompletionSource<MatchResult>(
                     TaskCreationOptions.RunContinuationsAsynchronously);
 
-                WaitingPlayer waitingPlayer = new WaitingPlayer
-                (
-                    playerIDToken,
-                    tcs,
-                    ct.Register(() => Cancel(playerIDToken))
-                );
-
-                _matchingQueue.Enqueue(waitingPlayer);
-                _waitingMap[playerIDToken] = waitingPlayer;
+                _waitingMap[playerIDToken] = new WaitingPlayer(playerIDToken, tcs,
+                    cancellationTokenRegist: ct.Register(() => Cancel(playerIDToken)));
+                
+                _matchingQueue.Enqueue(playerIDToken);
 
                 TryMatch();
 
@@ -165,49 +164,46 @@ namespace YoungManGomoku_WebServer.SingletoneManager
             while (_matchingQueue.Count >= 2)
             {
                 //_logger.LogTrace($"[{DateTime.Now}] Matching Queue Count : {_matchingQueue.Count}");
-                WaitingPlayer? p1 = _matchingQueue.Dequeue();
+                string p1Token = _matchingQueue.Dequeue();
 
-                if (!_waitingMap.ContainsKey(p1.PlayerIdToken))
+                if (_waitingMap.TryGetValue(p1Token, out WaitingPlayer? p1) == false)
                 {
-                    _logger.LogWarning($"[{DateTime.Now}] Matching Queue에는 있는데 대기자 맵에 없습니다. [{_serverManagerContext.GetPlayerUID(p1.PlayerIdToken)}]");
+                    _logger.LogWarning($"[{DateTime.Now}] Matching Queue에는 있는데 대기자 맵에 없습니다. [{_serverManagerContext.GetPlayerUID(p1Token)}]");
                     continue; // p1이 매칭을 취소해서 맵에 없으니 큐에서 버림
                 }
 
                 WaitingPlayer? p2 = null;
                 while (_matchingQueue.Count > 0)
                 {
-                    WaitingPlayer candidate = _matchingQueue.Dequeue();
-                    if (p1.PlayerIdToken == candidate.PlayerIdToken)
+                    string p2Token = _matchingQueue.Dequeue();
+                    
+                    if (p1Token == p2Token)
                     {
-                        _logger.LogWarning($"[{DateTime.Now}] Matching Queue에 자기 자신과 매칭, 데이터 스킵\n{_serverManagerContext.GetPlayerUID(candidate.PlayerIdToken)}");
+                        _logger.LogWarning($"[{DateTime.Now}] 중복 등록된 Matching queue 값이므로 스킵\n{_serverManagerContext.GetPlayerUID(p2Token)}");
                         continue;
                     }
 
 					// Queue에 있어도 _waitingMap에 존재해야만 매칭
-					if (_waitingMap.ContainsKey(candidate.PlayerIdToken))
+					if (_waitingMap.TryGetValue(p2Token, out p2))
                     {
-                        _logger.LogTrace($"[{DateTime.Now}] Matching 다른 플레이어 발견\n{_serverManagerContext.GetPlayerUID(candidate.PlayerIdToken)}");
-                        p2 = candidate;
+                        _logger.LogTrace($"[{DateTime.Now}] Matching 상대 플레이어 발견\n{_serverManagerContext.GetPlayerUID(p2Token)}");
                         break;
                     }
                 }
 
-                if (p2 == null)
+                if (p2 == null) // 큐를 끝까지 다 뽑았는데 p2를 찾지 못함
                 {
-                    // 매칭 큐가 0이 될 때까지 다 뽑았는데 상대가 없다고...? 큐에 취소한 이전의 나만 잔뜩 들어있던 거야?
                     _logger.LogDebug($"[{DateTime.Now}] Matching Fail - Matching Opponent is not Exist");
-                    _matchingQueue.Enqueue(p1);
-                    break; // 매칭 가능한 상대 없음
+                    _matchingQueue.Enqueue(p1Token); // p1Token은 유효했던 Queue이므로 되돌려놓기
+                    break; // 매칭 상대 찾기 종료
                 }
-
 
                 // 매칭 성공, 방 배정
                 ulong roomID = _serverManagerContext.GenerateUID64();
                 _logger.LogDebug($"[{DateTime.Now}] [Matching Success] Room ID {roomID}");
 
-
-                Random random = new Random();
-                int colorRandomValue = random.Next(0, 2);
+                
+                int colorRandomValue = Random.Next(0, 2);
 
                 // 매칭에 성공한 두 플레이어에게 각각의 게임을 위한 정보 (색, 방번호) 전달 및 클라이언트로 응답
                 p1.TaskCompSrc?.TrySetResult(
@@ -233,11 +229,18 @@ namespace YoungManGomoku_WebServer.SingletoneManager
                 _waitingMap.Remove(p1.PlayerIdToken);
                 _waitingMap.Remove(p2.PlayerIdToken);
 
-                _gameRoomManager.CreateRoom(
-                    roomID,
-                    colorRandomValue == 0 ? _serverManagerContext.GetPlayerUID(p1.PlayerIdToken) : _serverManagerContext.GetPlayerUID(p2.PlayerIdToken),
-                    colorRandomValue == 1 ? _serverManagerContext.GetPlayerUID(p1.PlayerIdToken) : _serverManagerContext.GetPlayerUID(p2.PlayerIdToken)
-                );
+                if (colorRandomValue == 0)
+                {
+                    _gameRoomManager.CreateRoom(roomID,
+                        _serverManagerContext.GetPlayerUID(p1.PlayerIdToken),
+                        _serverManagerContext.GetPlayerUID(p2.PlayerIdToken));
+                }
+                else
+                {
+                    _gameRoomManager.CreateRoom(roomID,
+                        _serverManagerContext.GetPlayerUID(p2.PlayerIdToken),
+                        _serverManagerContext.GetPlayerUID(p1.PlayerIdToken));
+                }
             }
         }
     }
